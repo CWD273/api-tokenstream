4e4f73e447e6f34f899c2f21f500058997f87841
CWD273 <itscwd273@gmail.com>
2025-11-16 20:16:38 -0600

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,8 +1,11 @@
 import { get } from "../lib/cache.js";
+import { info } from "../lib/logger.js";
 
 export default async function handler(req, res) {
   const { id } = req.query;
   const entry = id ? get(id) : null;
+
+  info("health_request", { id, hasCache: !!entry }, req);
 
   res.setHeader("Content-Type", "application/json");
   res.status(200).send(
